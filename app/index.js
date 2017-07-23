a89793430805777fc648ab74db679290633d1055
Matias Bogado <bogado.matias9191@gmail.com>
2017-07-23 16:52:34 -0300

    Setup react + webpack local environment

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const renderApp = () => {
+  ReactDOM.render(<div>TEST</div>, document.getElementById('app'));
+};
+
+renderApp();
---
+++ webpack.config.js
@@ -0,0 +1,261 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
+// replace localhost with 0.0.0.0 if you want to access
+// your app from wifi or a virtual machine
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
+const sourcePath = path.join(__dirname, './app');
+const buildDirectory = path.join(__dirname, './build');
+
+const stats = {
+  assets: true,
+  children: false,
+  chunks: false,
+  hash: false,
+  modules: false,
+  publicPath: false,
+  timings: true,
+  version: false,
+  warnings: true,
+  colors: {
+    green: '\u001b[32m'
+  }
+};
+
+module.exports = function(env) {
+  const nodeEnv = env && env.prod ? 'production' : 'development';
+  const isProd = nodeEnv === 'production';
+
+  const serviceWorkerBuild = env && env.sw;
+
+  let cssLoader;
+
+  const plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+      async: true,
+      children: true,
+      minChunks: 2
+    }),
+
+    // setting production environment will strip out
+    // some of the development code from the app
+    // and libraries
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+        TARGET: JSON.stringify('WEB')
+      }
+    }),
+
+    // create css bundle
+    new ExtractTextPlugin('style.css'),
+
+    // create index.html
+    new HtmlWebpackPlugin({
+      template: './index.ejs',
+      inject: true,
+      production: isProd,
+      minify: isProd && {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+    }),
+  ];
+
+  if (isProd) {
+    plugins.push(
+      // minify remove some of the dead code
+      new UglifyJSPlugin({
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true
+        }
+      })
+    );
+
+    cssLoader = ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2
+          }
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            config: './postcss.config.js'
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            outputStyle: 'collapsed',
+            sourceMap: true,
+            includePaths: [sourcePath]
+          }
+        }
+      ]
+    });
+  } else {
+    plugins.push(
+      // make hot reloading work
+      new webpack.HotModuleReplacementPlugin(),
+      // show module names instead of numbers in webpack stats
+      new webpack.NamedModulesPlugin(),
+      // don't spit out any errors in compiled assets
+      new webpack.NoEmitOnErrorsPlugin()
+    );
+
+    cssLoader = [
+      {
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 2
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          config: './postcss.config.js'
+        }
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          outputStyle: 'expanded',
+          sourceMap: true,
+          data: '@import "theme/variables";',
+          includePaths: [sourcePath]
+        }
+      }
+    ];
+  }
+
+  if (serviceWorkerBuild) {
+    plugins.push(
+      new SWPrecacheWebpackPlugin({
+        cacheId: 'budgeting-app',
+        filename: 'sw.js',
+        maximumFileSizeToCacheInBytes: 800000,
+        mergeStaticsConfig: true,
+        minify: true,
+        runtimeCaching: [
+          {
+            handler: 'cacheFirst',
+            urlPattern: /(.*?)/
+          }
+        ]
+      })
+    );
+  }
+
+  const entryPoint = isProd
+    ? './index.js'
+    : [
+      // activate HMR for React
+      'react-hot-loader/patch',
+
+      // bundle the client for webpack-dev-server and connect to the provided endpoint
+      `webpack-dev-server/client?http://${host}:${port}`,
+
+      // bundle the client for hot reloading only- means to only hot reload for successful updates
+      'webpack/hot/only-dev-server',
+
+      // the entry point of our app
+      './index.js'
+    ];
+
+  return {
+    devtool: isProd ? 'source-map' : 'cheap-module-source-map',
+    context: sourcePath,
+    entry: {
+      main: entryPoint
+    },
+    output: {
+      path: buildDirectory,
+      publicPath: '/',
+      filename: '[name]-[hash:8].js',
+      chunkFilename: 'chunk[name]-[chunkhash:8].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(html|jpe?g|png|ttf|woff2?)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: 'static/[name]-[hash:8].[ext]'
+            }
+          }
+        },
+        {
+          test: /\.(scss|css)$/,
+          exclude: /node_modules/,
+          use: cssLoader
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.(svg)$/,
+          exclude: /node_modules/,
+          use: 'raw-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+      modules: [path.resolve(__dirname, 'node_modules'), sourcePath]
+    },
+
+    plugins,
+
+    performance: isProd && {
+      maxAssetSize: 300000,
+      maxEntrypointSize: 300000,
+      hints: 'warning'
+    },
+
+    stats: stats,
+
+    devServer: {
+      contentBase: './app',
+      publicPath: '/',
+      historyApiFallback: true,
+      port: port,
+      host: host,
+      hot: !isProd,
+      compress: isProd,
+      stats: stats
+    }
+  };
+};
